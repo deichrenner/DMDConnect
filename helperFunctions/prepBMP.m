9f0b21d25d8148a9312d22b7418d7bc33be9fb00
deichrenner <k.hueck@gmx.de>
2016-03-09 16:12:45 +0100

    added enhanced run length encoding functionality to prepBMP. so far implemented is only inter-line compression. the line to line comparison is not implemented yet.

--- helperFunctions/prepBMP.m
+++ helperFunctions/prepBMP.m
@@ -14,9 +14,9 @@ signature = ['53'; '70'; '6C'; '64'];
 imageWidth = dec2hex(typecast(uint16(size(BMP1,2)),'uint8'),2);
 imageHeight = dec2hex(typecast(uint16(size(BMP1,1)),'uint8'),2);
 numOfBytes = dec2hex(typecast(uint32(size(BMP1,1)*size(BMP1,2)*...
-    bitDepth),'uint8'));
+    bitDepth),'uint8'),2);
 backgroundColor = ['00'; '00'; '00'; '00'];
-compression = '00';
+compression = '02';
 
 header = [signature; imageWidth; imageHeight; numOfBytes; ...
     'FF'; 'FF'; 'FF'; 'FF'; 'FF'; 'FF'; 'FF'; 'FF'; backgroundColor; ...
@@ -24,31 +24,32 @@ header = [signature; imageWidth; imageHeight; numOfBytes; ...
     '00'; '00'; '00'; '00'; '00'; '00'; '00'; '00'; '00'; '00'; '00';...
     '00'; '00'; '00'];
 
-BMP1 = BMP1'*1;
+% convert input matrix to decimal and transpose for later handling
+BMP1 = BMP1'*1; 
 
-%  expand to 24bit in 3x8bit decimal notation
-BMP3c = dec2hex([zeros(size(BMP1(:),1),2), BMP1(:)]',2); % add two more colors in order to build the full 24 bit bitmap
+% expand to 24bit in 3x8bit decimal notation
+BMP24 = dec2hex([zeros(size(BMP1(:),1),2), BMP1(:)]',2);
 
+% clear return variable
 data = '';
 
-% compress if whished
-if strcmp(compression, '01')
+% % compress if whished
+if strcmp(compression, '02')
     % reshape in order to get 24bit pixel information line by line
-    BMP24 = BMP3c(:);
-    test = reshape(BMP24, 3*size(BMP1,1), size(BMP1,2));
+    BMP24 = sprintf(BMP24');
+    BMP24 = regexp(BMP24, sprintf('\\w{1,%d}', 3*2), 'match')';
+    BMP24 = reshape(BMP24, size(BMP1,1), [])';
     for i = 1:size(BMP24,1)
-        ind = find(diff([BMP24(i,1)-1, BMP24(i,:)]));
-        relMat = [dec2hex(diff([ind, numel(BMP24(i,:))+1]),2), num2str(BMP24(i,ind)','%06d')];
-        for j = 1:size(relMat,1)
-            data = [data relMat(j,:)];
-        end
-        data = [data '0000']; % add the end of line command
+        [~, ~, ic] = unique(BMP24(i,:));
+        ind = find(diff([ic(1)-1, ic(:)']));
+        relMat = [formatRep(diff([ind, numel(BMP24(i,:))+1])), cell2mat(BMP24(i,ind)')];
+        data = [data sprintf(relMat(:,:)')];
     end
-    data = [data '0001']; % add the end of file command
-    data = [header; char(regexp(data, sprintf('\\w{1,%d}', 2), 'match')')];
+    % put together header, compressed data and end of image padding
+    data = [header; char(regexp(data, sprintf('\\w{1,%d}', 2), 'match')'); '00'; '01'; '00'];
 else
     % merge header and data
-    data = [header; BMP3c;]; % combine header and data
+    data = [header; BMP24;];
 end
 
 end
